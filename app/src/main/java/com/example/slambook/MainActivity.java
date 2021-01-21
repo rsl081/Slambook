08c7ab8200dc1972410b45345d91a8761aa7c410
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-01-21 10:32:10 +0800

    Update Image

--- app/src/main/java/com/example/slambook/MainActivity.java
+++ app/src/main/java/com/example/slambook/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dito_kayo_magtest_wrap_at_match_lagi);
+        setContentView(R.layout.add_entry);
     }
 }
